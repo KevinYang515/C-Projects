8e5727172c15b6a635624904e593f55ded3bb520
unknown <kevin850515123@gmail.com>
2020-08-15 01:29:49 +0800

    add leetcode226

--- leetcode/leetcode226.cpp
+++ leetcode/leetcode226.cpp
@@ -11,7 +11,6 @@ int main(){
     TreeNode *root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
     invertTree_i(root);
     invertTree_r(root);
-
     return 0;
 }
 
