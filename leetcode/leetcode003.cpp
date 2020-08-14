5e7dd5e9404dfe219f431319d35b78b040677f5a
unknown <kevin850515123@gmail.com>
2020-08-15 00:29:13 +0800

    leetcode003

---
+++ leetcode/leetcode003.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string s);
+int main() {
+    vector<string> s = {"abcabcbb", "bbbbb"};
+    string s1[2] = {"abcabcbb", "bbbbb"};
+
+    for (string s_temp : s){
+        printf("%d, ", lengthOfLongestSubstring(s_temp));
+    }
+}
+
+int lengthOfLongestSubstring(string s) {
+    const char* str = s.c_str();
+    int ans = 0;
+    map<char, int> my_map;
+    for (int i = 0, j = 0; j < s.length(); j ++){
+        if (my_map.find(str[j]) != my_map.end()){
+            i = max(i, my_map.find(str[j])->second);
+        }
+        ans = max(ans, j - i + 1);
+        my_map[str[j]] = j + 1;
+    }
+
+    return ans;
+}
