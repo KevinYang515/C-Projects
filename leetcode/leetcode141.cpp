575bb2797d35e58d68da7d4a1eb9c22b5b7edef3
unknown <kevin850515123@gmail.com>
2020-08-15 12:36:21 +0800

    add leetcode141

---
+++ leetcode/leetcode141.cpp
@@ -0,0 +1,34 @@
+#include "ListNode.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool hasCycle(ListNode *head);
+
+int main(){
+    vector<ListNode*> head_v = {new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4)))),
+                                new ListNode(1, new ListNode(2)),
+                                new ListNode(1),
+                                new ListNode(1, new ListNode(2))};
+    head_v[0]->next->next->next->next = head_v[0]->next;
+    head_v[1]->next->next = head_v[1];
+    for (ListNode* head : head_v){
+        printf("%s, ", hasCycle(head)?"true":"false");
+    }
+
+    return 0;
+}
+
+bool hasCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL) return false;
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while (slow != fast){
+        if (fast == NULL || fast->next == NULL) return false;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return true;
+}
