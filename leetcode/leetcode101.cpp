7246c941d851630012cb4e8c8bbe4eca68178ba7
KevinYang515 <KevinYang515>
2020-08-15 15:49:51 +0800

    add leetcode101

--- leetcode/leetcode101.cpp
+++ leetcode/leetcode101.cpp
@@ -21,13 +21,37 @@ int main(){
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 bool isSymmetric_r(TreeNode* root) {
-
+    helper_r(root, root);
 }
 
 bool helper_r(TreeNode* left, TreeNode* right){
-
+    if (left == NULL && right == NULL) return true;
+    if (left == NULL || right == NULL) return false;
+    return (left->val == right->val) && 
+           helper_r(left->left, right->right) &&
+           helper_r(left->right, right->left);
 }
 
+// Iterative
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 bool isSymmetric_i(TreeNode* root) {
+    queue<TreeNode*> q;
+    q.push(root);
+    q.push(root);
+    while (!q.empty()){
+        TreeNode* left = q.front();
+        q.pop();
+        TreeNode* right = q.front();
+        q.pop();
+        if (left == NULL && right == NULL) continue;
+        if (left == NULL || right == NULL) return false;
+        if (left->val != right->val) return false;
+        q.push(left->right);
+        q.push(right->left);
+        q.push(left->left);
+        q.push(right->right);
+    }
 
+    return true;
 }
